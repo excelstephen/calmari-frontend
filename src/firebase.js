5d094fcbf79f6891f1e93b98211ce012fa546922
Excel Stephen (The Tech Guy) <excelstephen1@gmail.com>
2025-05-22 09:08:31 +0100

    added .env

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB9XzB4YtZyUeAPouJxIhzRetJpyJRpaCE",
-  authDomain: "calmari-authentication.firebaseapp.com",
-  projectId: "calmari-authentication",
-  storageBucket: "calmari-authentication.firebasestorage.app",
-  messagingSenderId: "354893596045",
-  appId: "1:354893596045:web:d48f4070af69a58eb669c4",
-  measurementId: "G-YJX9W67HWL"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
